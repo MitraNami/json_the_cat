ae059f3af44e49402181b07d222907621917d44c
Mitra Nami <a.mxnami@gmail.com>
2020-09-23 17:00:55 +0000

    refactored and added test using macho and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,18 @@
 const request = require('request');
 
 
-const breedFetcher = function(breed) {
-  const api = `https://api.thecatapi.com/v1/breeds/search?q=${breed.slice(0, 4)}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const api = `https://api.thecatapi.com/v1/breeds/search?q=${breedName.slice(0, 4)}`;
   request(api, (error, response, body) => {
     if (error) {
-      console.log("Error", error);
-      return;
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      //when the breed is not found the data will be an empty array also, error = null
+      callback(error, data.length ? data[0].description : "Breed Not Found!");
     }
-    const data = JSON.parse(body);
-    //when the breed is not found the data will be an empty array
-    console.log(data.length ? data[0].description : "Breed Not Found!");
   });
 };
 
-const breed = process.argv[2];
-breedFetcher(breed);
 
-
-
+module.exports = {fetchBreedDescription};
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,33 @@
+const {assert} = require("chai");
+const {fetchBreedDescription} = require('../breedFetcher');
+
+
+describe("#fetchBreedDescription", () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      //we expect no error fot this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. ";
+      
+      //compare returned description
+      assert.equal(expectedDesc, desc);
+
+      done();
+
+    });
+  });
+
+  it('returns a string "Breed Not Found!" for an invalid breed', (done) => {
+    fetchBreedDescription('mnmonm', (err, desc) => {
+      //we expact no error
+      assert.equal(err, null);
+
+      const expectedDesc = "Breed Not Found!";
+      assert.equal(expectedDesc, desc);
+      
+      done();
+    })
+  });
+
+});
