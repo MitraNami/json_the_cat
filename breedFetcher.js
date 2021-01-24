246f62c2e110392a2f74954dbfa1add654f85688
Mitra Nami <49569563+MitraNami@users.noreply.github.com>
2021-01-24 16:36:08 -0500

    Update breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,28 @@
 const request = require('request');
 
 
-const fetchBreedDescription = function(breedName, callback) {
-  const api = `https://api.thecatapi.com/v1/breeds/search?q=${breedName.slice(0, 4)}`;
-  request(api, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-    } else {
-      const data = JSON.parse(body);
-      if (data.length) {
-        callback(null, data[0].description );
-      } else {
-        callback("Breed Not Found!", null);
+const fetchBreedDescription = (breed, callback) => {
+
+  const uri = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  request(uri, (err, res, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    if (res.statusCode === 200) {
+      const [breedinfo] = JSON.parse(body);
+      if (breedinfo) {
+        const breedDes = breedinfo.description;
+        callback(null, breedDes);
+        return;
       }
+      callback('Breed Not Found!', null);
+    } else {
+      callback(res.statusCode, null);
     }
   });
+
 };
 
 
-module.exports = {fetchBreedDescription};
+module.exports = fetchBreedDescription;
